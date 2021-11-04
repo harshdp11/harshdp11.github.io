2facca81aa6f785ce45b8fac664eaf46d1b237ef
Harshdeep Singh <harshdeepsingh@Harshdeeps-MacBook-Pro.local>
2021-11-04 17:11:26 +0530

    svdvf

--- interaction.js
+++ interaction.js
@@ -149,11 +149,10 @@ function myFunction() {
   }
   prevScrollpos = currentScrollPos;
   
-  console.log('fdfdd');
   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
   var scrolled = (winScroll / height) * 100;
-  console.log(scrolled);
+
   document.getElementById("progressBar").style.width = scrolled + "%";
 }
 
